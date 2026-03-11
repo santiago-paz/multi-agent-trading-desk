6e248aab30fcc9ade06c745b4c54ea91c6c0decb
Santiago Paz <santiago.paz.1992@gmail.com>
2026-03-11 00:30:45 -0300

    Add middleware for Basic Authentication
    - Implemented middleware to enforce Basic Authentication for all routes except specified exclusions (API routes, static files, and favicon).
    - Configured middleware to check for authentication credentials in request headers and respond with a 401 status if not provided.

---
+++ src/middleware.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
+
+export function middleware(req: NextRequest) {
+  // If Basic Auth is not configured, skip
+  if (!process.env.BASIC_AUTH_USER || !process.env.BASIC_AUTH_PASSWORD) {
+    return NextResponse.next();
+  }
+
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      pwd === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse('Auth Required.', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  });
+}
